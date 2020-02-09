839ab84c2104f88c610f3ccfaec4d6420ac32a41
Jordan Christensen <jordan.c4922@gmail.com>
2020-02-09 13:04:32 -0700

    Update git ignore add initial xcode project

---
+++ Todo/Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Jordan Christensen on 2/9/20.
+//  Copyright © 2020 Mazjap Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
